9e0a300e7c2d365b810fde4aff0af01626618823
bblks <168915058+bblks@users.noreply.github.com>
2024-07-26 19:21:48 +0300

    Update devm.py

--- ZeMusic/plugins/play/devm.py
+++ ZeMusic/plugins/play/devm.py
@@ -11,11 +11,16 @@ from config import OWNER_ID, LOGGER_ID
 import config
 
 @app.on_message(command(["مطور", "المطور"]))
-)
-async def huhh(client: Client, message: Message):
+async def devid(client: Client, message: Message):
+    usr = await client.get_users(OWNER_ID)
+    name = usr.first_name
+    usrnam = usr.username
+    uid = OWNER_ID
+    await app.download_media(usr.photo.big_file_id, file_name=os.path.join("downloads", "developer.jpg"))
+       
     await message.reply_photo(
         photo="downloads/developer.jpg",
-        caption = f"""<b>  𝒘𝒆𝒍𝒄𝒐𝒎𝒆𝒖 𝒕𝒐 <b>\n<a href="https://t.me/ngd_1"> 𝒔𝒐𝒖𝒓𝒄𝒆 𝒏𝒂𝒋𝒅 🇸🇦</a></b>"""",
+        caption=f"""<b>𝑛𝑎𝑚𝑒 :</b> <a href='tg://user?id={uid}'>{name}</a>\n\n<b>𝑢𝑠𝑒𝑟 :</b> @{usrnam}""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
