75d78c8c3dd33b05eb4fa9aa1dc46162f07ef3f6
bblks <168915058+bblks@users.noreply.github.com>
2024-10-29 00:01:13 +0300

    Update start.py

--- ZeMusic/plugins/bot/start.py
+++ ZeMusic/plugins/bot/start.py
@@ -121,9 +121,9 @@ async def welcome(client, message: Message):
                 except:
                     pass
             if member.id == app.id:
-                if message.chat.type != ChatType.SUPERGROUP:
-                    await message.reply_text(_["start_4"])
-                    return await app.leave_chat(message.chat.id)
+                #if message.chat.type != ChatType.SUPERGROUP:
+                    #await message.reply_text(_["start_4"])
+                    #return await app.leave_chat(message.chat.id)
                 if message.chat.id in await blacklisted_chats():
                     await message.reply_text(
                         _["start_5"].format(
@@ -136,7 +136,16 @@ async def welcome(client, message: Message):
                     return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
-
+                await message.reply_photo(
+                    photo=config.START_IMG_URL,
+                    caption=_["start_3"].format(
+                        message.from_user.first_name,
+                        app.mention,
+                        message.chat.title,
+                        app.mention,
+                    ),
+                    reply_markup=InlineKeyboardMarkup(out),
+                )
                 await add_served_chat(message.chat.id)
                 await message.stop_propagation()
         except Exception as ex:
