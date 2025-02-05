d4ff7b78693788d5c0ad86196469a1744f98a999
bblks <168915058+bblks@users.noreply.github.com>
2025-02-05 19:41:54 +0300

    Update سجل الجروبات.py

--- ZeMusic/plugins/play/سجل الجروبات.py
+++ ZeMusic/plugins/play/سجل الجروبات.py
@@ -1,88 +1,36 @@
-import random
+# This code is written by (C) TheTeamAlexa bot will send message to log group when someone add
+# this bot to new group make sure to star all projects
+# Copyright (C) 2021-2022 by Alexa_Help@ Github, < TheTeamAlexa >.
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# All rights reserved. © Alisha © Alexa © Yukki
+
 from pyrogram import Client, filters
-from pyrogram.types import (
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    InputMediaPhoto,
-    InputMediaVideo,
-    Message,
-)
+from pyrogram.types import Message
 from ZeMusic import app
 from ZeMusic.utils.database import get_served_chats
-from config import OWNER_ID, LOGGER_ID
+from config import LOGGER_ID
 
-photo_urls = [
-    "https://envs.sh/Wi_.jpg",
-    "https://envs.sh/Wi_.jpg",
-    "https://envs.sh/Wi_.jpg",
-    "https://envs.sh/Wi_.jpg",
-    "https://envs.sh/Wi_.jpg",
-]
 
-@app.on_message(filters.new_chat_members & filters.group)
-async def welcome_new_member(client: Client, message: Message):
-    chat = message.chat
-    dev_id = OWNER_ID
-    for new_member in message.new_chat_members:
-        if new_member.id == dev_id:
-            chat_id = message.chat.id
-            #user_mention = new_member.mention
-            #await client.send_message(chat_id, f"مرحبًا بك يا {user_mention} في المجموعة!")
+async def lul_message(chat_id: int, message: str):
+    await app.send_message(chat_id=chat_id, text=message)
 
-            info = await app.get_chat(dev_id)
-            name = info.first_name
-            markup = InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton(name, user_id=dev_id)]
-                ]
-            )
 
-            photos = []
-            async for photo in client.get_chat_photos(dev_id, limit=1):
-                
-                photos.append(photo)
-
-            if not photos:
-                await message.reply_text(
-                    f"↢ مرحباً مطوري <a href='tg://user?id={dev_id}'>{name}</a> نورت الشات ياعزيزي🧸",
-                    reply_markup=markup
-                )
-            else:
-                await message.reply_photo(
-                    photos[0].file_id,
-                    caption=f"↢ مرحباً مطوري <a href='tg://user?id={dev_id}'>{name}</a> نورت الشات ياعزيزي🧸",
-                    reply_markup=markup
-                )
-
-    # جزء التعامل مع إضافة البوت إلى مجموعة جديدة
+@app.on_message(filters.new_chat_members)
+async def on_new_chat_members(client: Client, message: Message):
     if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
         added_by = message.from_user.first_name if message.from_user else "مستخدم غير معروف"
         added_id = message.from_user.id
 
         matlabi_jhanto = message.chat.title
         served_chats = len(await get_served_chats())
-        cont = await app.get_chat_members_count(chat.id)
-        chatusername = (message.chat.username if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐆ʀᴏᴜ𝑝")
-        lemda_text = (
-            f"🌹 تمت إضافة البوت إلى مجموعة جديدة.\n\n"
-            f"┏━━━━━━━━━━━━━━━━━┓\n"
-            f"┣★ <b>𝙲𝙷𝙰𝚃</b> › : {matlabi_jhanto}\n"
-            f"┣★ <b>𝙲𝙷𝙰𝚃 𝙸𝙳</b> › : {chat.id}\n"
-            f"┣★ <b>𝙲𝙷𝙰𝚃 𝚄𝙽𝙰𝙼𝙴</b> › : @{chatusername}\n"
-            f"┣★ <b>𝙲𝙾𝚄𝙽𝚃</b> › : {cont}\n"
-            f"┣★ <b>𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂</b> › : {served_chats}\n"
-            f"┣★ <b>𝙰𝙳𝙳𝙴𝙳 𝙱𝚈</b> › :\n"
-            f"┗━━━ꪜ <a href='tg://user?id={added_id}'>{added_by}</a>"
-        )
+        chat_id = message.chat.id
+
+        chat = await client.get_chat(int(chat_id))
+        cont = chat.members_count
         
-        
-        await app.send_photo(
-            LOGGER_ID,
-            photo=random.choice(photo_urls),
-            caption=lemda_text,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton(f"{added_by}", url=f"tg://openmessage?user_id={added_id}")]
-                ]
-            )
-        ) 
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
+        else:
+            chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+        lemda_text = f"🌹 تمت اضافه البوت الى مجموعه جديدة .\n\n┏━━━━━━━━━━━━━━━━━┓\n┣★ <b>𝙲𝙷𝙰𝚃</b> › : {matlabi_jhanto}\n┣★ <b>𝙲𝙷𝙰𝚃 𝙸𝙳</b> › : {chat_id}\n┣★ <b>𝙲𝙷𝙰𝚃 𝚄𝙽𝙰𝙼𝙴</b> › : {chatusername}\n┣★ <b>𝙲𝙾𝚄𝙽𝚃</b> › : {cont}\n┣★ <b>𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃</b> › : {served_chats}\n┣★ <b>𝙰𝙳𝙳𝙴𝙳 𝙱𝚈</b> › :\n┗━━━ꪜ <a href='tg://user?id={added_id}'>{added_by}</a>"
+        await lul_message(LOGGER_ID, lemda_text)
